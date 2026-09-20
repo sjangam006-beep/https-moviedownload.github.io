ef657f3c43499478affde73dd50e90fba0ac5b94
sjangam006-beep <sjangam006@gmail.com>
2026-09-20 20:26:28 +0530

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,21 @@
+const movies = [
+    {
+        title: "My Movie",
+        year: "2026",
+        language: "Hindi",
+        poster: "https://example.com/poster.jpg",
+        watch: "https://example.com/watch",
+        download: "https://example.com/download",
+        description: "Movie description"
+    },
+
+    {
+        title: "Another Movie",
+        year: "2026",
+        language: "English",
+        poster: "https://example.com/poster2.jpg",
+        watch: "https://example.com/watch2",
+        download: "https://example.com/download2",
+        description: "Another movie"
+    }
+];
